a08ffde0b7ec7073f27f77e1d7a51f92f67fedf6
Delon Newman <contact@delonnewman.name>
2024-10-31 23:21:48 -0400

    Add flesh to Listener

--- lib/slippers/listener.rb
+++ lib/slippers/listener.rb
@@ -4,11 +4,25 @@ require 'drb/drb'
 
 module Slippers
   class Listener
-    def greet
-      'hey'
+    def self.start(name:)
+      new(name:).start
+    end
+
+    def initialize(name:)
+      @name = name
+      @port = format('%04d', rand(9999))
+      @uri = "druby://localhost:#@port"
+      @registry = Registry.new
+    end
+
+    def start
+      @registry.add(uri: @uri, name: @name)
+      DRb.start_service(@uri, self)
+      DRb.thread.join
+    end
+
+    def class_index
+      ClassList::Index.new
     end
   end
 end
-
-DRb.start_service('druby://localhost:3141', Slippers::Listener.new)
-DRb.thread.join
